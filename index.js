0dfea27f50c8724954fbcaa31fb69b1e3b4583a8
Erin Shields <elshields92@gmail.com>
2020-04-21 22:15:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -60,7 +60,8 @@ function placeOrder(cardNumber) {
     return "Sorry, we don't have a credit card on file for you."
   } else {
     //console.log("Your total cost is $" + total() + ", which will be charged to the card " + cardNumber + ".")
-    return `Your total cost is ${total()}, which will be charged to the card ${cardNumber}.`
     cart = []
+    return `Your total cost is ${total()}, which will be charged to the card ${cardNumber}.`
+    
   }
 }
