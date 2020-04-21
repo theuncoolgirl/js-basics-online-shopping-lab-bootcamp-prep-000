21241390c8ae5a36b4361e78736f6a22d35a305c
Erin Shields <elshields92@gmail.com>
2020-04-21 22:13:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -59,8 +59,7 @@ function placeOrder(cardNumber) {
   if (!cardNumber) {
     return "Sorry, we don't have a credit card on file for you."
   } else {
-    var cartTotal = total()
-    console.log("Your total cost is $" + cartTotal + ", which will be charged to the card " + cardNumber + ".")
+    console.log("Your total cost is $" + total() + ", which will be charged to the card " + cardNumber + ".")
     cart = []
   }
 }
