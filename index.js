cd4f2b6cb8996fc83634b0a29f0377d0d651230e
Erin Shields <elshields92@gmail.com>
2020-04-21 22:39:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -50,7 +50,7 @@ function removeFromCart(item) {
       cart.splice(i, 1);
       return cart
     } else {
-      console.log("That item is not in your cart.")
+      return "That item is not in your cart."
     }
   }
 }
