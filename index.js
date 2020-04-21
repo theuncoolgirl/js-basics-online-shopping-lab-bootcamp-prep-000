f221924f1a8f76339a7ab888e4cba144559552f5
Erin Shields <elshields92@gmail.com>
2020-04-21 22:37:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -55,21 +55,13 @@ function removeFromCart(item) {
   }
 }
 
-//function placeOrder(cardNumber) {
-//  if (!cardNumber) {
-//    return "Sorry, we don't have a credit card on file for you."
-//  } else {
-    //console.log("Your total cost is $" + total() + ", which will be charged to the card " + cardNumber + ".")
-//    console.log(`Your total cost is ${total()}, which will be charged to the card ${cardNumber}.`)
-//    cart = []
-//  }
-//}
-
-function placeOrder(creditCard) {
-  if (!creditCard) {
-    console.log("We don't have a credit card on file for you to place your order.");
+function placeOrder(cardNumber) {
+  if (!cardNumber) {
+    return "Sorry, we don't have a credit card on file for you."
   } else {
-    console.log("Your total cost is $" + total() + ", which will be charged to the card " + creditCard + ".");
-    cart = [];
+    //console.log("Your total cost is $" + total() + ", which will be charged to the card " + cardNumber + ".")
+    const yourTotal = total()
+    cart = []
+    return `Your total cost is ${yourTotal}, which will be charged to the card ${cardNumber}.`)
   }
 }
